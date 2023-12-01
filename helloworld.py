f233dcaa0ebe9e12496495a708e6224a649d18c8
Dr. Gabor FACSKO <59848559+gfacsko@users.noreply.github.com>
2023-12-01 16:29:44 +0100

    Levelező tagozat, 2023. december 1
    A levelező tagozaton tartott pót óra segédprogramjai: Hello World!, változók, listák, irányítási szerkezetek, string kezelés. A másodfokú egyenlet megoldása. Egy "Lórum ipsé"-vel generált szöveg elemzése, a latin ABC kis betűinek eloszlása a szövegben, barchart készítése, grafika, ábrázolás.

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+#
+# Segédprogramok a "(VH-MIT009) Python programozás alapok" c. tárgyhoz
+# Developed by Gabor FACSKO (facsko.gabor@uni-milton.hu)
+# Milton Friedman University, Budapest, 2023
+# --------------------------------------------------------------------------
+#
+
+# Kiírás
+#print ("Szia Világ!")
